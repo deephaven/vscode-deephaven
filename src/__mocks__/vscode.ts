e1d57e1438c2685f4d2b47dc432b5ddc646f43c7
Brian Ingles <brianingles@deephaven.io>
2024-08-09 18:06:09 -0500

    Comment (#79-2)

--- src/__mocks__/vscode.ts
+++ src/__mocks__/vscode.ts
@@ -1,3 +1,8 @@
+/**
+ * Mock `vscode` module. Note that `vi.mock('vscode')` has to be explicitly
+ * called in any test module needing to use this mock. It will not be loaded
+ * automatically.
+ */
 import { vi } from 'vitest';
 
 export const workspace = {
