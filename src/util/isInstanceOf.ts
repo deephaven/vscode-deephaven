dd301b3c44c0fdb5aec485732563a1056856cf6a
Brian Ingles <brianingles@deephaven.io>
2024-10-02 14:58:02 -0500

    comment (#79-2)

--- src/util/isInstanceOf.ts
+++ src/util/isInstanceOf.ts
@@ -1,6 +1,7 @@
 /**
- * Type guard wrapper of `instanceof` operator. This is purely so we can mock
- * instanceof checks in unit tests since `instanceof` is already a type guard.
+ * Type guard wrapper of `instanceof` operator. This is not actually necessary
+ * as a typeguard, since `instanceof` is already a typeguard, but it allows us
+ * to mock `instanceof` checks in unit tests.
  * @param value The value to check
  * @param type The type to check against
  * @returns `true` if `value` is an instance of `type`
