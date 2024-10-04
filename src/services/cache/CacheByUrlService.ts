f63d1a93d0f8c381b428260eef72c60e7d18267d
Brian Ingles <brianingles@deephaven.io>
2024-10-04 16:33:35 -0500

    Moved method (#79)

--- src/services/cache/CacheByUrlService.ts
+++ src/services/cache/CacheByUrlService.ts
@@ -14,6 +14,18 @@ export class CacheByUrlService<TValue> implements ICacheService<URL, TValue> {
   private readonly _loader: (url: URL) => Promise<TValue>;
   private readonly _promiseMap = new URLMap<Promise<TValue>>();
 
+  get = async (url: URL): Promise<TValue> => {
+    if (!this._promiseMap.has(url)) {
+      this._promiseMap.set(url, this._loader(url));
+    }
+
+    return this._promiseMap.get(url)!;
+  };
+
+  invalidate = (url: URL): void => {
+    this._promiseMap.delete(url);
+  };
+
   dispose = async (): Promise<void> => {
     const promises = [...this._promiseMap.values()];
     this._promiseMap.clear();
@@ -27,17 +39,5 @@ export class CacheByUrlService<TValue> implements ICacheService<URL, TValue> {
     });
 
     await Promise.all(disposing);
-  };
-
-  get = async (url: URL): Promise<TValue> => {
-    if (!this._promiseMap.has(url)) {
-      this._promiseMap.set(url, this._loader(url));
-    }
-
-    return this._promiseMap.get(url)!;
-  };
-
-  invalidate = (url: URL): void => {
-    this._promiseMap.delete(url);
   };
 }
