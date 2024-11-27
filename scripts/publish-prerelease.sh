abda4eba3c9d33cf6084232e8e047551172feb0a
Brian Ingles <brianingles@deephaven.io>
2024-11-27 15:56:57 -0600

    Fixed issue with tag name + not committing to git (v0.1.15)

--- scripts/publish-prerelease.sh
+++ scripts/publish-prerelease.sh
@@ -5,11 +5,19 @@ set -e
 # Determine current npm version and calculate next patch version
 current=$(node -p -e "require('./package.json').version")
 next=$(npx semver $current -i patch)
+tag="v$next-pre"
 
-# Create a new branch, update the version, and create a new tag
-git checkout -b v$next-pre
+# Create a new branch
+git checkout -b $tag
+
+# Update version
 npm version --git-tag-version=false $next
-git tag $next-pre
+
+# Commit and tag the new version
+git add package.json
+git add package-lock.json
+git commit -m "$tag"
+git tag $tag
 git push --tags
 
 # Publish a pre-release version of the extension to the Marketplace
