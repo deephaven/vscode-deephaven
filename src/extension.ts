c62aaa19cdedcc11a27f6fce9f5f9ef003545d46
Brian Ingles <brianingles@deephaven.io>
2025-01-21 11:26:13 -0600

    Removed NODE_TLS_REJECT_UNAUTHORIZED hack (DH-18086)

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 import { ExtensionController } from './controllers';
 import { ConfigService } from './services';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 export function activate(context: vscode.ExtensionContext): void {
   const controller = new ExtensionController(context, ConfigService);
 
