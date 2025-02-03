a44cf120a5ea9901fa8dfe0a089881a5159cd054
Brian Ingles <brianingles@deephaven.io>
2025-02-03 13:25:29 -0600

    Removed pushd (DH-18428)

--- scripts/e2e-docker.sh
+++ scripts/e2e-docker.sh
@@ -1,6 +1,4 @@
-pushd "e2e"
-
 set -e
 
-docker compose build --progress plain --build-arg NODE_VERSION=20.15.1
-docker compose up --abort-on-container-exit --exit-code-from e2e
+docker compose --project-directory e2e build --progress plain --build-arg NODE_VERSION=20.15.1
+docker compose --project-directory e2e up --abort-on-container-exit --exit-code-from e2e
