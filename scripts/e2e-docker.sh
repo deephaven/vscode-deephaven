40447cfd45f85d3993176201740a2a463619f2e3
Brian Ingles <brianingles@deephaven.io>
2025-02-04 12:28:14 -0600

    docker compose args (DH-18428)

--- scripts/e2e-docker.sh
+++ scripts/e2e-docker.sh
@@ -1,4 +1,4 @@
 set -e
 
-docker compose --project-directory e2e build --progress plain --build-arg NODE_VERSION=20.15.1
+docker compose --project-directory e2e --progress plain build --build-arg NODE_VERSION="20.15.1"
 docker compose --project-directory e2e up --abort-on-container-exit --exit-code-from e2e
