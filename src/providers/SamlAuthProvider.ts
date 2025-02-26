c746c85de41aac588b4ed7bfcf5f59482cbcb869
Brian Ingles <brianingles@deephaven.io>
2025-02-26 17:23:24 -0600

    Cleanup (DH-18583)

--- src/providers/SamlAuthProvider.ts
+++ src/providers/SamlAuthProvider.ts
@@ -3,7 +3,13 @@ import {
   type AuthenticatedClient as DheAuthenticatedClient,
   type UnauthenticatedClient,
 } from '@deephaven-enterprise/auth-nodejs';
-import { Logger, makeSAMLSessionKey, parseSamlScopes, uniqueId } from '../util';
+import {
+  Logger,
+  makeSAMLSessionKey,
+  parseSamlScopes,
+  rejectAfterTimeout,
+  uniqueId,
+} from '../util';
 import {
   DH_SAML_AUTH_PROVIDER_TYPE,
   DH_SAML_LOGIN_URL_SCOPE_KEY,
@@ -60,6 +66,47 @@ export class SamlAuthProvider
     return dheClient as unknown as DheAuthenticatedClient;
   };
 
+  /**
+   * The SAML auth flow will go through a series of redirects eventually sending
+   * a redirect to the vscode extension via the result of `createSamlRedirectUrl`.
+   * This method is responsible for validating the incoming request and verifying
+   * it has the expected state id as sent in the original `redirectUrl` to DH
+   * server.
+   * @param serverUrl The DH server URL this handler is for.
+   * @param uriEventHandler The uri event handler to use for handling the redirect.
+   * @param disposables An array of disposables to add the uri event handler to.
+   * @returns A promise that resolves to true if the uri event was handled successfully.
+   */
+  static handleUriEventForServer = (
+    serverUrl: URL,
+    uriEventHandler: UriEventHandler,
+    disposables: vscode.Disposable[]
+  ): Promise<true> => {
+    return new Promise<true>((resolve, reject) => {
+      uriEventHandler.event(
+        async uri => {
+          logger.debug('Handling uri:', uri.toString());
+
+          const state = SamlAuthProvider.pendingAuthState.get(serverUrl);
+          if (state == null) {
+            reject(`No state found for ${uri}.`);
+            return;
+          }
+
+          const stateId = uri.path.substring(1);
+          if (stateId !== state.stateId) {
+            reject(`State id mismatch: ${stateId}`);
+            return;
+          }
+
+          resolve(true);
+        },
+        undefined,
+        disposables
+      );
+    });
+  };
+
   constructor(context: vscode.ExtensionContext) {
     this._context = context;
 
@@ -86,6 +133,17 @@ export class SamlAuthProvider
   private readonly _disposable: vscode.Disposable;
   private readonly _uriEventHandler = new UriEventHandler();
 
+  /**
+   * Create a redirectUrl to send to the DH server as part of SAML auth flow.
+   * This will be used to redirect back to VS Code once the user has logged in.
+   * The `stateId` is encoded in the url as the path that can be extracted by
+   * a UriEventHandler to validate the redirect.
+   * Note that I attempted to pass this via a query param, but things were not
+   * getting encoded / decoded correctly on DH server. Simpler to just pass as
+   * the path.
+   * @param stateId
+   * @returns The redirect URL to send to the DH server.
+   */
   createSamlRedirectUrl(stateId: UniqueID): URL {
     const publisher = this._context.extension.packageJSON.publisher;
     const name = this._context.extension.packageJSON.name;
@@ -105,6 +163,12 @@ export class SamlAuthProvider
   getSessions = async (
     _scopes?: readonly string[]
   ): Promise<readonly vscode.AuthenticationSession[]> => {
+    // Not implementing this since there really isn't a concept of persistent
+    // SAML sessions that we can store in the extension. Once the user disconnects,
+    // there's no way to re-use the session since the extension only has access
+    // to the short lived nonce used to login. IdPs often store session info in
+    // browser, so user can just re-login, and if they have an active session,
+    // the browser will just pass it along without requiring credentials again.
     return [];
   };
 
@@ -157,45 +221,25 @@ export class SamlAuthProvider
           vscode.Uri.parse(samlLoginUrl.toString())
         );
 
-        const subscriptions: vscode.Disposable[] = [];
+        const disposables: vscode.Disposable[] = [];
 
         return Promise.race([
-          new Promise<true>((resolve, reject) => {
-            this._uriEventHandler.event(
-              async uri => {
-                logger.debug('Handling uri:', uri.toString());
-
-                const state = SamlAuthProvider.pendingAuthState.get(serverUrl);
-                if (state == null) {
-                  reject(`No state found for ${uri}.`);
-                  return;
-                }
-
-                const stateId = uri.path.substring(1);
-                if (stateId !== state.stateId) {
-                  reject(`State id mismatch: ${stateId}`);
-                  return;
-                }
-
-                resolve(true);
-              },
-              undefined,
-              subscriptions
-            );
-          }),
-          new Promise<never>((_, reject) =>
-            setTimeout(() => reject('Cancelled by timeout.'), 60000)
+          SamlAuthProvider.handleUriEventForServer(
+            serverUrl,
+            this._uriEventHandler,
+            disposables
           ),
+          rejectAfterTimeout(60000, 'Cancelled by timeout.'),
           new Promise<never>((_, reject) =>
             cancellationToken.onCancellationRequested(
               () => reject('Cancelled by user.'),
               undefined,
-              subscriptions
+              disposables
             )
           ),
         ]).finally(() => {
-          subscriptions.forEach(subscription => subscription.dispose());
-          subscriptions.length = 0;
+          disposables.forEach(subscription => subscription.dispose());
+          disposables.length = 0;
         });
       }
     );
@@ -221,13 +265,20 @@ export class SamlAuthProvider
 
     const session: vscode.AuthenticationSession = {
       id: uniqueId(),
-      accessToken: samlSessionKey,
+      // our "access token" is a short lived nonce, so no reason to hold on to it here
+      accessToken: '',
       account: {
         id: userInfo.username,
         label: userInfo.username,
       },
       scopes,
     };
+
+    this._onDidChangeSessions.fire({
+      added: [session],
+      removed: [],
+      changed: [],
+    });
 
     return session;
   };
