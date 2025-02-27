309613371cac1f288f9ba3c23ae9c83b2deb51c1
Brian Ingles <brianingles@deephaven.io>
2025-02-27 09:45:44 -0600

    Tests (DH-18583)

---
+++ src/dh/dhe.spec.ts
@@ -0,0 +1,84 @@
+import { describe, it, expect, vi } from 'vitest';
+import type { EnterpriseClient } from '@deephaven-enterprise/jsapi-types';
+import {
+  AUTH_CONFIG_CUSTOM_LOGIN_CLASS_SAML_AUTH,
+  AUTH_CONFIG_PASSWORDS_ENABLED,
+  AUTH_CONFIG_SAML_LOGIN_URL,
+  AUTH_CONFIG_SAML_PROVIDER_NAME,
+} from '../common';
+import { getDheAuthConfig } from './dhe';
+
+describe('getDheAuthConfig', () => {
+  const given = {
+    samlLoginClass: [
+      AUTH_CONFIG_CUSTOM_LOGIN_CLASS_SAML_AUTH,
+      'mock.loginClass',
+    ],
+    samlProviderName: [AUTH_CONFIG_SAML_PROVIDER_NAME, 'mock.providerName'],
+    samlLoginUrl: [AUTH_CONFIG_SAML_LOGIN_URL, 'mock.loginUrl'],
+    passwordsEnabled: [AUTH_CONFIG_PASSWORDS_ENABLED, 'true'],
+    passwordsDisabled: [AUTH_CONFIG_PASSWORDS_ENABLED, 'false'],
+  } as const;
+
+  const givenSamlConfig = {
+    full: [given.samlLoginClass, given.samlProviderName, given.samlLoginUrl],
+    partial: [given.samlLoginClass, given.samlProviderName],
+  } as const;
+
+  const expected = {
+    samlConfigFull: {
+      loginClass: given.samlLoginClass[1],
+      providerName: given.samlProviderName[1],
+      loginUrl: given.samlLoginUrl[1],
+    },
+  } as const;
+
+  it.each([
+    [
+      'Undefined passwords config, Full SAML config',
+      givenSamlConfig.full,
+      {
+        isPasswordEnabled: true,
+        samlConfig: expected.samlConfigFull,
+      },
+    ],
+    [
+      'Undefined password config, Partial SAML config',
+      givenSamlConfig.partial,
+      {
+        isPasswordEnabled: true,
+        samlConfig: null,
+      },
+    ],
+    [
+      'Passwords enabled config, Full SAML config',
+      [given.passwordsEnabled, ...givenSamlConfig.full],
+      {
+        isPasswordEnabled: true,
+        samlConfig: expected.samlConfigFull,
+      },
+    ],
+    [
+      'Passwords disabled config, Full SAML config',
+      [given.passwordsDisabled, ...givenSamlConfig.full],
+      {
+        isPasswordEnabled: false,
+        samlConfig: expected.samlConfigFull,
+      },
+    ],
+    [
+      'Passwords disabled config, Partial SAML config',
+      [given.passwordsDisabled, ...givenSamlConfig.partial],
+      {
+        isPasswordEnabled: false,
+        samlConfig: null,
+      },
+    ],
+  ])('should return auth config: %s', async (_label, given, expected) => {
+    const getAuthConfigValues = vi.fn().mockResolvedValue(given);
+    const dheClient = { getAuthConfigValues } as unknown as EnterpriseClient;
+
+    const actual = await getDheAuthConfig(dheClient);
+    expect(actual).toEqual(expected);
+  });
+});
