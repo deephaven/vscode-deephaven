60ebb173978f7d267b45da3df28c002ef45a5703
Brian Ingles <brianingles@deephaven.io>
2025-03-03 11:15:10 -0600

    Fixed paths (DH-18428-2)

--- e2e-testing/src/panels.spec.ts
+++ e2e-testing/src/panels.spec.ts
@@ -1,11 +1,9 @@
 import { EditorView, VSBrowser, Workbench } from 'vscode-extension-tester';
-import fs from 'node:fs';
 import path from 'node:path';
 import { getCodeLens, openResources, openTextEditor } from './testUtils';
 
 const testWsPath = path.resolve(__dirname, '..', 'test-ws/');
 const simpleTicking3 = 'simple_ticking3.py';
-const simpleTicking3Path = path.resolve(testWsPath, simpleTicking3);
 
 describe('Panels Tests', () => {
   let timerMs = 0;
@@ -14,16 +12,15 @@ describe('Panels Tests', () => {
     timerMs = performance.now();
     await new EditorView().closeAllEditors();
 
-    // eslint-disable-next-line no-console
-    console.log(
-      'Path exists:',
-      simpleTicking3Path,
-      fs.existsSync(simpleTicking3Path)
-    );
-
     // Open script in 2 different tab groups
     // eslint-disable-next-line no-console
-    console.log('Opening resources:', testWsPath, simpleTicking3Path);
+    console.log('Opening resources:', testWsPath, simpleTicking3);
+
+    await openResources(testWsPath, simpleTicking3);
+
+    await new Workbench().executeCommand('View: Split Editor Down');
+
+    await VSBrowser.instance.takeScreenshot('panels');
   });
 
   after(async () => {
@@ -34,12 +31,6 @@ describe('Panels Tests', () => {
   });
 
   it('should open panels', async () => {
-    await openResources(testWsPath, simpleTicking3Path);
-
-    await new Workbench().executeCommand('View: Split Editor Down');
-
-    await VSBrowser.instance.takeScreenshot('panels');
-
     const editor = await openTextEditor(simpleTicking3);
     const runDhFile = await getCodeLens(editor, 'Run Deephaven File');
 
