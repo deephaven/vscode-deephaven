57ee34ea8b66be6c086fce89c3966c72a1cf88f4
Brian Ingles <brianingles@deephaven.io>
2025-03-10 08:29:48 -0500

    Mocha ctrf report (DH-18428-2)

--- e2e-testing/src/mocharc.ts
+++ e2e-testing/src/mocharc.ts
@@ -2,6 +2,10 @@ import { MochaOptions } from 'vscode-extension-tester';
 
 const options: MochaOptions = {
   timeout: 30000,
+  reporter: 'mocha-ctrf-json-reporter',
+  reporterOptions: {
+    outputDir: 'test-reports',
+  },
 };
 
 module.exports = options;
