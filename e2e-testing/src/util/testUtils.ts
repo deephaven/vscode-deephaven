93cbf57334fd7296772f8e5e73afc3899dc0034a
Brian Ingles <brianingles@deephaven.io>
2025-03-11 14:21:01 -0500

    Try clearing file input (DH-18428-2)

--- e2e-testing/src/util/testUtils.ts
+++ e2e-testing/src/util/testUtils.ts
@@ -112,10 +112,9 @@ export async function openFileResources(
     const openItem = await fileMenu.getItem('Open File...');
     await openItem!.select();
     const input = await InputBox.create();
+    await input.clear();
     await input.setText(filePath);
     await input.confirm();
-
-    await VSBrowser.instance.driver.sleep(1000);
   }
 }
 
