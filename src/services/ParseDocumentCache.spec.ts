867c3d5b29f66eff9676ae56eed55527a6c46f23
Brian Ingles <brianingles@deephaven.io>
2025-04-02 15:07:30 -0500

    typo (DH-18882)

--- src/services/ParseDocumentCache.spec.ts
+++ src/services/ParseDocumentCache.spec.ts
@@ -9,7 +9,7 @@ interface MockParsedDocument {
   label: `mock.parsed.${string}`;
 }
 
-const mockDock = {
+const mockDoc = {
   av1: mockDocument('fileA', 1),
   av2: mockDocument('fileA', 2),
   parseDocument: (document: vscode.TextDocument): MockParsedDocument =>
@@ -20,10 +20,10 @@ const mockDock = {
 
 const mockEvent = {
   av1: mockT<vscode.TextDocumentChangeEvent>({
-    document: mockDock.av1,
+    document: mockDoc.av1,
   }),
   av2: mockT<vscode.TextDocumentChangeEvent>({
-    document: mockDock.av2,
+    document: mockDoc.av2,
   }),
 };
 
@@ -42,7 +42,7 @@ function initializeCache(initialCachedDoc?: vscode.TextDocument): {
   if (initialCachedDoc != null) {
     const actual = cache.get(initialCachedDoc);
     expect(parseDocument).toHaveBeenCalledWith(initialCachedDoc);
-    expect(actual).toEqual(mockDock.parseDocument(initialCachedDoc));
+    expect(actual).toEqual(mockDoc.parseDocument(initialCachedDoc));
   }
 
   return { cache, changeHandler, closeHandler };
@@ -66,39 +66,39 @@ function getHandlers(): {
 
 beforeEach(() => {
   vi.clearAllMocks();
-  parseDocument.mockImplementation(mockDock.parseDocument);
+  parseDocument.mockImplementation(mockDoc.parseDocument);
 });
 
 it('should re-cache if document changed to newer version or explicitly requested', () => {
-  const { cache, changeHandler } = initializeCache(mockDock.av1);
+  const { cache, changeHandler } = initializeCache(mockDoc.av1);
 
   // Document changed to newer version
   vi.clearAllMocks();
   changeHandler(mockEvent.av2);
-  expect(parseDocument).toHaveBeenCalledWith(mockDock.av2);
-  expect(cache.get(mockDock.av2)).toEqual(mockDock.parseDocument(mockDock.av2));
+  expect(parseDocument).toHaveBeenCalledWith(mockDoc.av2);
+  expect(cache.get(mockDoc.av2)).toEqual(mockDoc.parseDocument(mockDoc.av2));
 
   // Document changed to older version
   vi.clearAllMocks();
   changeHandler(mockEvent.av1);
   expect(parseDocument).not.toHaveBeenCalled();
 
-  expect(cache.get(mockDock.av1)).toEqual(mockDock.parseDocument(mockDock.av1));
-  expect(parseDocument).toHaveBeenCalledWith(mockDock.av1);
+  expect(cache.get(mockDoc.av1)).toEqual(mockDoc.parseDocument(mockDoc.av1));
+  expect(parseDocument).toHaveBeenCalledWith(mockDoc.av1);
 });
 
 it('should clear cache on document close', () => {
-  const { cache, closeHandler } = initializeCache(mockDock.av1);
+  const { cache, closeHandler } = initializeCache(mockDoc.av1);
 
-  expect(parseDocument).toHaveBeenCalledWith(mockDock.av1);
+  expect(parseDocument).toHaveBeenCalledWith(mockDoc.av1);
 
   // Request cached version
   vi.clearAllMocks();
-  cache.get(mockDock.av1);
+  cache.get(mockDoc.av1);
   expect(parseDocument).not.toHaveBeenCalled();
 
   // Close document and request a again
-  closeHandler(mockDock.av1);
-  cache.get(mockDock.av1);
-  expect(parseDocument).toHaveBeenCalledWith(mockDock.av1);
+  closeHandler(mockDoc.av1);
+  cache.get(mockDoc.av1);
+  expect(parseDocument).toHaveBeenCalledWith(mockDoc.av1);
 });
