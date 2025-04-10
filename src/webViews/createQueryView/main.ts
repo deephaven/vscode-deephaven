2458ebe007d9bafa2b0c2142de29cfe973fb65d7
Brian Ingles <brianingles@deephaven.io>
2025-04-10 13:49:10 -0500

    Import post msg constants (DH-17600)

--- src/webViews/createQueryView/main.ts
+++ src/webViews/createQueryView/main.ts
@@ -1,8 +1,4 @@
-// TODO: Update esbuild to support ESM so we can import this directly
-const DEEPHAVEN_POST_MSG = {
-  authTokenRequest: 'io.deephaven.message.LoginOptions.authTokenRequest',
-  workerCreated: 'io.deephaven.message.LoginOptions.workerCreated',
-} as const;
+import { DEEPHAVEN_POST_MSG } from '../../common/postMsgConstants';
 
 const FROM_DEEPHAVEN = new Set([
   DEEPHAVEN_POST_MSG.authTokenRequest,
