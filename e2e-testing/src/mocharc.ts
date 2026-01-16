51c557aa751514fd42dd4ae810d481e1282f6356
Brian Ingles <brianingles@deephaven.io>
2026-01-16 13:43:39 -0600

    Increased the mocha slow setting since e2e is always "slow" (DH-20440)

--- e2e-testing/src/mocharc.ts
+++ e2e-testing/src/mocharc.ts
@@ -2,6 +2,7 @@ import { MochaOptions } from 'vscode-extension-tester';
 
 const options: MochaOptions = {
   timeout: 30000,
+  slow: 20000,
   reporter: 'mocha-multi-reporters',
   reporterOptions: {
     reporterEnabled: 'spec, mocha-ctrf-json-reporter',
