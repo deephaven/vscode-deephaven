065a37d878a8ff9875d9b4030c4f08bc8405cfb0
gzh2003 <germainzhanghoule@gmail.com>
2026-01-19 14:40:08 -0500

    Improved logging

--- src/webViews/variablePanel/main.ts
+++ src/webViews/variablePanel/main.ts
@@ -23,27 +23,27 @@ window.addEventListener(
 
   // From DH iframe -> VS Code
   if (isLoginOptionsRequest(data)) {
-    logger.info('Received login options request');
+    logger.info('Received login options request from iframe');
     vscode.postMessage({ data });
     return;
   }
 
   if (isSessionDetailsRequest(data)) {
-    logger.info('Received session details request');
+    logger.info('Received session details request from iframe');
     vscode.postMessage({ data });
     return;
   }
 
   // From VS Code -> DH iframe
   if (isLoginOptionsResponse(data)) {
-    logger.info('Received login response');
+    logger.info('Received login response from ext');
     const iframeWindow = getIframeContentWindow();
     iframeWindow.postMessage(data.payload, data.targetOrigin);
     return;
   }
 
   if (isSessionDetailsResponse(data)) {
-    logger.info('Received session details');
+    logger.info('Received session details from ext');
     const iframeWindow = getIframeContentWindow();
     iframeWindow.postMessage(data.payload, data.targetOrigin);
     return;
