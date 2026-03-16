7297012c2a74ffa40d43747ec781486b1985c356
Brian Ingles <brianingles@deephaven.io>
2026-03-16 11:25:01 -0500

    DH-21980_python-managed-servers/task-01: Add TypeScript types for Python Environments API to extensionApiUtils.ts. Currently only has MsPythonExtensionApi interface for old ms-python.python API. Need to add PythonEnvironmentApi interface matching the new ms-python.vscode-python-envs extension API including: PythonEnvironment, PythonEnvironmentId, PythonEnvironmentExecutionInfo, Package, PackageId interfaces and getEnvironment, getPackages, onDidChangePackages methods.
    Added PythonEnvironmentApi interface with getEnvironment, getPackages, onDidChangePackages methods; added supporting interfaces PythonEnvironment, PythonEnvironmentExecutionInfo, PythonEnvironmentId, Package, PackageId, DidChangePackagesEventArgs, PackageChangeKind enum, GetEnvironmentScope type, and getPythonEnvironmentApi() getter function to extensionApiUtils.ts. TypeScript compiles with no errors. (#DH-21980)

--- src/util/extensionApiUtils.ts
+++ src/util/extensionApiUtils.ts
@@ -3,12 +3,82 @@ import type { ExtensionInfo, ExtensionVersion, McpVersion } from '../types';
 import { uniqueId } from './idUtils';
 
 const MS_PYTHON_EXTENSION_ID = 'ms-python.python';
+const MS_PYTHON_ENVS_EXTENSION_ID = 'ms-python.vscode-python-envs';
 
 /** Microsoft Python extension api */
 interface MsPythonExtensionApi {
   environments: {
     getActiveEnvironmentPath: () => Promise<{ path: string }>;
   };
+}
+
+/** Options for executing a Python executable */
+interface PythonCommandRunConfiguration {
+  executable: string;
+  args?: string[];
+}
+
+/** Execution details for a Python environment */
+interface PythonEnvironmentExecutionInfo {
+  run: PythonCommandRunConfiguration;
+  activatedRun?: PythonCommandRunConfiguration;
+  activation?: PythonCommandRunConfiguration[];
+  deactivation?: PythonCommandRunConfiguration[];
+}
+
+/** Unique identifier for a Python environment */
+interface PythonEnvironmentId {
+  id: string;
+  managerId: string;
+}
+
+/** A Python environment from the ms-python.vscode-python-envs extension */
+interface PythonEnvironment {
+  readonly envId: PythonEnvironmentId;
+  readonly name: string;
+  readonly displayName: string;
+  readonly displayPath: string;
+  readonly version: string;
+  readonly environmentPath: vscode.Uri;
+  readonly execInfo: PythonEnvironmentExecutionInfo;
+  readonly sysPrefix: string;
+  readonly description?: string;
+}
+
+/** Unique identifier for a Python package */
+interface PackageId {
+  id: string;
+  managerId: string;
+  environmentId: string;
+}
+
+/** A Python package from the ms-python.vscode-python-envs extension */
+interface Package {
+  readonly pkgId: PackageId;
+  readonly name: string;
+  readonly displayName: string;
+  readonly version?: string;
+  readonly description?: string;
+}
+
+export enum PackageChangeKind {
+  add = 'add',
+  remove = 'remove',
+}
+
+/** Arguments for the onDidChangePackages event */
+interface DidChangePackagesEventArgs {
+  environment: PythonEnvironment;
+  changes: { kind: PackageChangeKind; pkg: Package }[];
+}
+
+export type GetEnvironmentScope = undefined | vscode.Uri;
+
+/** Python Environments extension API (ms-python.vscode-python-envs) */
+interface PythonEnvironmentApi {
+  getEnvironment(scope: GetEnvironmentScope): Promise<PythonEnvironment | undefined>;
+  getPackages(environment: PythonEnvironment): Promise<Package[] | undefined>;
+  onDidChangePackages: vscode.Event<DidChangePackagesEventArgs>;
 }
 
 /** Create ExtensionInfo from the ExtensionContext */
@@ -52,5 +122,14 @@ export function getMsPythonExtensionApi():
   | undefined {
   return vscode.extensions.getExtension<MsPythonExtensionApi>(
     MS_PYTHON_EXTENSION_ID
+  );
+}
+
+/** Get the Python Environments extension api (ms-python.vscode-python-envs) */
+export function getPythonEnvironmentApi():
+  | vscode.Extension<PythonEnvironmentApi>
+  | undefined {
+  return vscode.extensions.getExtension<PythonEnvironmentApi>(
+    MS_PYTHON_ENVS_EXTENSION_ID
   );
 }
