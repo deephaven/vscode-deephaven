d53633bc84817477ff96c274c5d7dd978ba6ae64
Brian Ingles <brianingles@deephaven.io>
2026-04-27 17:36:36 -0500

    Increasing e2e timeout (#DH-20578)

--- e2e-testing/src/mocharc.ts
+++ e2e-testing/src/mocharc.ts
@@ -1,8 +1,8 @@
 import { MochaOptions } from 'vscode-extension-tester';
 
 const options: MochaOptions = {
-  timeout: 30000,
-  slow: 20000,
+  timeout: 60000,
+  slow: 40000,
   reporter: 'mocha-multi-reporters',
   reporterOptions: {
     reporterEnabled: 'spec, mocha-ctrf-json-reporter',
